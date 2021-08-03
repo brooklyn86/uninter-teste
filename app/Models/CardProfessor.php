75a023f0b0c232e9a26defaa59f009625cdcc9fa
Victor Hugo <victorhugoraj@gmail.com>
2021-08-02 23:54:08 -0300

    Criando a base do Projeto

---
+++ app/Models/CardProfessor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CardProfessor extends Model
+{
+    public $table = 'card_professor';
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => '/'], function () {
+	Route::get('/', ['as' => 'produto.cadastro', 'uses' => 'DashboardController@index']);
+
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
